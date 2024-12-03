1e6cf0c22062ec0f7d6ffacc1ba64cbd030b5193
vicente silva <vicentesilvala@gmail.com>
2024-12-02 19:42:15 -0700

    front pre ia

---
+++ src/app/pages/pagina-predicciones/pagina-predicciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-predicciones',
+  templateUrl: './pagina-predicciones.component.html',
+  styleUrls: ['./pagina-predicciones.component.css']
+})
+export class PaginaPrediccionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
