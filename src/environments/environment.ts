eb91cdcb40ed520e6eade0eca070a40d35ac163d
vicentesilv <vicentesilvala@gmail.com>
2024-12-16 23:27:36 -0600

    final2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,10 @@ export const environment = {
   production: false,
   endpoint: 'http://localhost:3001/'
 };
-
+// export const environment = {
+//   production: false,
+//   endpoint: '192.168.1.79:3001/'
+// };
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
